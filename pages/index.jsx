e3e18e69a62a514d57b3f497366d6e66ae3ded01
Mitch Smith <mitchpsmith1998@gmail.com>
2020-08-27 21:18:11 +1000

    fix text contrast and remove unused image

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="flex flex-col bg-gray-900 h-screen w-screen items-center justify-center p-20">
         <div className="w-full md:w-1/2 ">
           <img alt="logo" className="mx-auto" src="/logo.svg" />
-          <p className="text-center text-orange-400 text-3xl mt-16">Welcome to NextJS-Wabbit!</p>
+          <p className="text-center text-orange-600 text-3xl mt-16">Welcome to NextJS-Wabbit!</p>
           <a className="text-white block w-full text-center text-lg" href="https://github.com/mitchbne/" rel="noopener noreferrer" target="_blank">@mitchbne</a>
         </div>
       </div>
