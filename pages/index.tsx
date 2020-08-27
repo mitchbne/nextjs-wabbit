a69934f83552747f182c8abe3e12a9f711293c15
Mitch Smith <mitchpsmith1998@gmail.com>
2020-08-27 21:38:17 +1000

    add more typescript config and update starter page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 import React, { Fragment } from "react"
 import Head from "next/head"
 
-export default function Home() {
+export default function Home () {
   return (
     <Fragment>
       <Head>
         <title>Next-WabbitJS</title>
         <link href="/favicon.ico" rel="icon" />
       </Head>
-
-      <div className="flex flex-col bg-gray-900 h-screen w-screen items-center justify-center p-20">
-        <div className="w-full md:w-1/2 ">
-          <img alt="logo" className="mx-auto" src="/logo.svg" />
-          <p className="text-center text-orange-600 text-3xl mt-16">Welcome to NextJS-Wabbit!</p>
-          <a className="text-white block w-full text-center text-lg" href="https://github.com/mitchbne/" rel="noopener noreferrer" target="_blank">@mitchbne</a>
+      <div className="flex flex-col items-center justify-center w-screen h-screen py-20 px-4 bg-gray-050">
+        <div className="w-full max-w-2xl ">
+          <img alt="logo" className="mx-auto h-12" src="https://tailwindui.com/img/tailwindui-logo.svg" />
+          <p className="mt-4 text-2xl text-center text-teal-600">Welcome to NextJS Wabbit!</p>
+          <a className="block w-full text-base text-center text-gray-600" href="https://github.com/mitchbne/" rel="noopener noreferrer" target="_blank">@mitchbne</a>
         </div>
       </div>
     </Fragment>
