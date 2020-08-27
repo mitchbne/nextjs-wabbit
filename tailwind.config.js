e786dca331bd54c547a93a3933e8c73173b83f5f
Mitch Smith <mitchpsmith1998@gmail.com>
2020-08-27 21:16:56 +1000

    upgrade tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,23 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  purge: [
-    "./pages/**/*.jsx",
-    "./components/**/*.jsx",
-  ],
+  purge: {
+    mode: "all",
+    content: [
+      "./**/*.ts",
+      "./**/*.jx",
+      "./**/*.tsx",
+      "./**/*.jsx",
+    ],
+  },
+  future: "all",
   theme: {
     extend: {
-      colors: { blue: { "400": "red" } },
-      // screens: {
-      // print: { raw: "print" }, // use utilities like print:text-black
-      // dark: { raw: "(prefers-color-scheme: dark)" }, // use utilities like dark:text-white
-      // },
-      height: { "96": "32rem" },
       fontFamily: { sans: ["Inter var", ...defaultTheme.fontFamily.sans] },
+      screens: {
+      // print: { raw: "print" }, // use utilities like print:text-black
+        dark: { raw: "(prefers-color-scheme: dark)" }, // use utilities like dark:text-white
+      },
     },
     container: { center: true },
   },
